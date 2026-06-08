38e11e4c701460424f75fd8595d12752f8ab78d6
Bharath Sivanesh.S <132342144+Bharathsivanesh@users.noreply.github.com>
2026-06-08 23:55:36 +0530

    stacj leetcode problem

--- src/DSA/Stack/RemoveAdjacent.java
+++ src/DSA/Stack/RemoveAdjacent.java
@@ -1,11 +1,13 @@
-import java.util.*;
-public class Main
-{
+package DSA.Stack;
+
+import java.util.Stack;
+
+public class RemoveAdjacent {
     //1047
     public static void main(String[] args)
     {
         String data="abbaac";
-        Stack<Character>st=new Stack<>();
+        Stack<Character> st=new Stack<>();
         for(char c:data.toCharArray())
         {
             if(st.isEmpty())
