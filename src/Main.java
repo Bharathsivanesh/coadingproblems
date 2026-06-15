9e44fe6bd2da0339640d4d9b029b2978cfc263b8
Bharath Sivanesh.S <132342144+Bharathsivanesh@users.noreply.github.com>
2026-06-15 23:41:27 +0530

    arrays

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,24 @@
 import java.util.*;
-public class Main
-{
-    public static void main(String[] args)
-    //Find Missing Number
-    {
-        int[]a={1,3};
-        int n=a.length+1;
-        int expectedsum=n*(n+1)/2;
-        int sum=0;
-        for(int num:a)
-        {
-            sum+=num;
-        }
-        System.out.print(expectedsum-sum);
 
+public class Main {
+    public static void main(String[] args) {
+
+        int[] a = {1,9,5,8,2,9,10};
+
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+
+        for(int num : a) {
+
+            if(num > max1) {
+                max2 = max1;
+                max1 = num;
+            }
+            else if(num > max2 && num != max1) {
+                max2 = num;
+            }
+        }
+
+        System.out.println("Second Largest = " + max2);
     }
 }
