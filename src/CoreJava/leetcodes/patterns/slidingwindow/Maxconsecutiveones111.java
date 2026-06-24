d975e17fc6fcd5f41a8daa6161fdefefd67d28a9
Bharath Sivanesh.S <132342144+Bharathsivanesh@users.noreply.github.com>
2026-06-24 23:52:16 +0530

    sliding window

---
+++ src/CoreJava/leetcodes/patterns/slidingwindow/Maxconsecutiveones111.java
@@ -0,0 +1,41 @@
+package CoreJava.leetcodes.patterns.slidingwindow;
+
+public class Maxconsecutiveones111 {
+
+    public static int longestOnes(int[] nums, int k) {
+
+        int left = 0;
+        int zeroCount = 0;
+        int max = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+
+            if (nums[right] == 0) {
+                zeroCount++;
+            }
+
+            while (zeroCount > k) {
+
+                if (nums[left] == 0) {
+                    zeroCount--;
+                }
+
+                left++;
+            }
+
+            max = Math.max(max, right - left + 1);
+        }
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0};
+        int k = 2;
+
+        int result = longestOnes(nums, k);
+
+        System.out.println("Maximum Consecutive Ones = " + result);
+    }
+}
