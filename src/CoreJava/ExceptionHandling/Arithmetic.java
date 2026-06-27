941ca0b22d09b859508510728f6af9dd0a69d406
Bharath Sivanesh.S <132342144+Bharathsivanesh@users.noreply.github.com>
2026-06-27 23:18:26 +0530

    exception

--- src/CoreJava/ExceptionHandling/Arithmetic.java
+++ src/CoreJava/ExceptionHandling/Arithmetic.java
@@ -6,6 +6,8 @@ public class Arithmetic {
 
         try {
             int res = 10 / 0;
+            System.out.println(res);
+
         } catch (ArithmeticException e) {
             System.out.println("Cannot divide by zero.");
         }
